202bf1057613d5989d1bcc7122ba14e2c46b70da
harry-urek <hariombhardwaj038@gmail.com>
2024-02-16 18:06:38 +0530

    webSock manager completed

--- app/ws.py
+++ app/ws.py
@@ -1,18 +1,26 @@
-from typing import List
-from fastapi import WebSocket
+from typing import List, Dict
+from fastapi import WebSocket, WebSocketDisconnect
+
+# An instance  ConnectionManager() gets initiated for every webSock endpoints
+# Each user connected to that webSock endpoint will be managed by the connection manager for that
+# endpoint with each user having a websock connection stored in @connections{uid : connection} & @connected [connections]
 
 
 class ConnectionManager:
     def __init__(self):
-        self.connections: List[WebSocket] = []
+        self.connections: Dict[int:WebSocket] = {}
 
-    async def connect(self, websock: WebSocket):
+    async def connect(self, user_id: int, session_id: int, websock: WebSocket):
         await websock.accept()
-        self.connections.append(websock)
+        self.connections[user_id] = websock
 
     async def broadcast_message(self, data: str):
-        for connection in self.connections:
-            await connection.send_text(data)
+        for _, connects in self.connections:
+            await connects.send_text(data)
 
-    async def disconnect(self, sock: WebSocket):
-        pass
+    def total_connections(self, websock: WebSocket):
+        return len(self.connections)
+
+    async def disconnect(self, uid: int, websock: WebSocket):
+        await websock.close()
+        del self.connections[uid]
