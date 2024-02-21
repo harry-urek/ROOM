2dfbe952889926184c1b3b87abadd9f3cfd1d4a1
harry-urek <hariombhardwaj038@gmail.com>
2024-02-21 20:56:30 +0530

    updated models to sqlalchemy 2.0 models

--- app/models/new_model.py
+++ app/models/new_model.py
@@ -1,5 +1,6 @@
 from uuid import uuid4
 from datetime import datetime
+from pytz import timezone
 from sqlalchemy import (
     mapped_column,
     Integer,
@@ -10,102 +11,106 @@ from sqlalchemy import (
     DateTime,
 )
 
+from sqlalchemy import func
 from sqlalchemy.exc import IntegrityError, NoResultFound
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import relationship, Mapped, mapped_mapped_column
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 
 from app.services.db import Base
+from typing import List, Optional, Set
 
 
 class TimeModel(Base):
     __abstract__ = True
 
-    created_t: Mapped[DateTime] = mapped_mapped_column(
-        default=datetime.now(), nullable=False)
-    update_t: Mapped[DateTime] = mapped_mapped_column(onupdate=datetime.now())
+    created_at: Mapped[Optional[datetime.datetime]] = mapped_column(
+        DateTime, server_default=func.now())
+    update_time: Mapped[Optional[datetime]] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+    )
 
 
 class UserModel(TimeModel):
     __tablename__ = "users"
 
-    uid: Mapped[int] = mapped_column(primary_key=True,
-                                     autoincrement=True, index=True)
-    nick_name: Mapped[str] = mapped_column(String)
-    name: Mapped[str] = mapped_column(String, nullable=False)
-    public_key: Mapped[str] = mapped_column(String)
-    email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
-    number: Mapped[str] = mapped_column(String, unique=True, nullable=False)
-    status: Mapped[str] = mapped_column(String)
+    uid: Mapped[int] = mapped_column(
+        primary_key=True, autoincrement=True, index=True)
+    nick_name: Mapped[str]
+    name: Mapped[str] = mapped_column(nullable=False)
+    public_key: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True, nullable=False)
+    number: Mapped[str] = mapped_column(unique=True, nullable=False)
+    status: Mapped[str]
 
     # One to One relationShip with the private Key
     # public_key = relationship("KeyModel", uselist=False, back_populates="user")
 
-    rooms = relationship("RoomModel", secondary="membership",
-                         back_populates="members")
+    rooms: Mapped[Set["RoomModel"]] = relationship(secondary="membership", back_populates="members"
+                                                   )
 
-    sessions = relationship(
-        "SessionModel", secondary="session_data", back_populates="users"
-    )
+    sessions: Mapped[Set["SessionModel"]] = relationship(secondary="session_data", back_populates="users"
+                                                         )
 
 
 class SessionModel(TimeModel):
     __tablename__ = "sessions"
 
-    session_id: Mapped[int] = mapped_column(Integer, primary_key=True,
-                                            autoincrement=True, index=True)
-    room_id: Mapped[int] = mapped_column(Integer, ForeignKey("rooms.rid"))
-    session_name: Mapped[int] = mapped_column(String, nullable=False)
-
-    room = relationship("RoomModel", back_populates="sessions")
-    users = relationship(
-        "UserModel", secondary="session_data", back_populates="sessions"
+    session_id: Mapped[int] = mapped_column(
+        primary_key=True, autoincrement=True, index=True
     )
+    session_name: Mapped[int] = mapped_column(nullable=False)
+
+    rooms: Mapped[Set["RoomModel"]] = relationship(back_populates="sessions")
+    users: Mapped[Set["UserModel"]] = relationship(secondary="session_data", back_populates="sessions"
+                                                   )
     # Establish a one - to - many relation with message sent in the room
-    messages = relationship(
-        "MessageModel",
-        back_populates="session",
+    messages: Mapped[List["MessageModel"]] = relationship(
+        back_populates="session"
     )
 
 
 class User_Session(Base):
     __tablename__ = "session_data"
 
-    session_id: Mapped[int] = mapped_column(Integer, ForeignKey(
-        "sessions.session_id"), primary_key=True)
+    session_id: Mapped[int] = mapped_column(
+        ForeignKey("sessions.session_id"), primary_key=True
+    )
 
     user_id: Mapped[int] = mapped_column(
-        Integer, ForeignKey("users.uid"), primary_key=True)
+        ForeignKey("users.uid"), primary_key=True)
 
 
 class Member_Model(Base):
     __tablename__ = "membership"
 
     room_id: Mapped[int] = mapped_column(
-        Integer, ForeignKey("rooms.rid"), primary_key=True)
+        ForeignKey("rooms.rid"), primary_key=True)
     user_id: Mapped[int] = mapped_column(
-        Integer, ForeignKey("users.uid"), primary_key=True)
+        ForeignKey("users.uid"), primary_key=True)
 
 
 class RoomModel(TimeModel):
     __tablename__ = "rooms"
 
-    room_name: Mapped[int] = mapped_column(String, nullable=False)
-    rid: Mapped[int] = mapped_column(Integer, primary_key=True,
-                                     autoincrement=True, index=True)
-    members = relationship(
-        "UserModel", secondary="membership", back_populates="rooms")
-    sessions = relationship("SessionModel", back_populates="room")
+    room_name: Mapped[str] = mapped_column(nullable=False)
+    rid: Mapped[int] = mapped_column(
+        primary_key=True, autoincrement=True, index=True)
+    members: Mapped[Set["UserModel"]] = relationship(
+        secondary="membership", back_populates="rooms")
+    session: Mapped["SessionModel"] = relationship(
+        back_populates="rooms")
 
 
 class MessageModel(TimeModel):
     __tablename__ = "messages"
 
-    mid: Mapped[int] = mapped_column(Integer, primary_key=True,
-                                     index=True, autoincrement=True)
+    mid: Mapped[int] = mapped_column(
+        primary_key=True, index=True, autoincrement=True)
 
-    sender_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.uid"))
-    mssg_encrypt: Mapped[int] = mapped_column(String)
-
-    session_id: Mapped[int] = mapped_column(
-        Integer, ForeignKey("sessions.session_id"))
-    session = relationship("SessionModel", back_populates="messages")
+    sender_id: Mapped[int] = mapped_column(
+        ForeignKey("users.uid"), nullable=False)
+    mssg_encrypt: Mapped[str]
+    room_id: Mapped[int] = mapped_column(
+        ForeignKey("rooms.rid"), nullable=False)
+    session_id: Mapped[int] = mapped_column(ForeignKey("sessions.session_id"))
+    room: Mapped["RoomModel"] = relationship(back_populates="messages")
